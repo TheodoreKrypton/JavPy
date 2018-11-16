255a2aa017418e454a0b126bf1c6f480767a03b6
TheodoreKrypton <624722398@qq.com>
2018-11-16 14:26:53 +0800

    enable cache policy for get_new_released
    Signed-off-by: echen11 <edward.chen@morningstar.com>

--- functions/new.py
+++ functions/new.py
@@ -1,10 +1,23 @@
 from sources.javmost_com import JavMostCom
+import datetime
 
 
 class New:
     def __init__(self):
         pass
 
-    @staticmethod
-    def get_newly_released(allow_many_actresses, up_to):
-        return JavMostCom.get_newly_released(allow_many_actresses, up_to)
+    newly_released = []
+    record_date = datetime.datetime.today()
+
+    @classmethod
+    def get_newly_released(cls, allow_many_actresses, up_to):
+        today = datetime.datetime.today()
+        if cls.record_date < today:
+            cls.record_date = today
+            cls.newly_released = []
+
+        if up_to <= len(cls.newly_released):
+            return cls.newly_released[:up_to]
+
+        else:
+            cls.newly_released = JavMostCom.get_newly_released(allow_many_actresses, up_to)
