caa8430590b871106645a061956d8e93e1490978
TheodoreKrypton <624722398@qq.com>
2018-11-27 06:07:42 +0800

    add search page

--- app/web/src/components/mainpages/preview.vue
+++ app/web/src/components/mainpages/preview.vue
@@ -1,14 +1,41 @@
 <template>
-  <div>Hello World.</div>
+  <div>
+    <el-card v-for="(video, i) in av" :key="i" style="width: 20%;" :body-style="{ padding: '0px' }" shadow="hover">
+      <img :src="video.preview_img_url" class="image" alt="preview">
+        <div class="bottom">
+          <table>
+            <tr>
+              <td style="width: 70%; text-align: left">
+                {{video.code}}
+              </td>
+              <td>
+                <el-button type="primary" plain style="float: right;">主要按钮</el-button>
+              </td>
+            </tr>
+          </table>
+        </div>
+    </el-card>
+  </div>
 </template>
 
 
 <script>
 export default {
-    name: 'preview'
+    name: 'preview',
+    props: [
+        'av'
+    ]
 }
 </script>
 
 <style lang="less" scoped>
+  .bottom {
+    margin-top: 10px;
+    line-height: 12px;
+  }
 
+  .image {
+    width: 100%;
+    display: block;
+  }
 </style>
--- app/web/src/components/mainpages/search.vue
+++ app/web/src/components/mainpages/search.vue
@@ -1,62 +1,57 @@
 <template>
-  <div>
-    <el-form ref="form" :model="form" :inline="true" @submit="search_by_code">
-      <el-form-item>
-        <el-input v-model="form.jav_code" placeholder="Jav Code"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-input v-model="form.actress" placeholder="Actress"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="search_by_code">Search!</el-button>
-        <el-button @click="clear">Clear</el-button>
-      </el-form-item>
-    </el-form>
-    <preview></preview>
-  </div>
+    <div>
+        <el-form ref="form" :model="form" :inline="true" @submit="search_by_code">
+            <el-form-item>
+                <el-input v-model="form.jav_code" placeholder="Jav Code"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input v-model="form.actress" placeholder="Actress"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="search_by_code">Search!</el-button>
+                <el-button @click="clear">Clear</el-button>
+            </el-form-item>
+        </el-form>
+        <preview :av="to_be_previewed"></preview>
+    </div>
 </template>
 
 <script>
 
-import axios from 'axios';
-import preview from './preview';
+    import axios from 'axios';
+    import preview from './preview';
 
-export default {
-    name: 'search',
-    components: {
-     preview
-    },
-    data() {
-      return {
-        form: {
-          jav_code: '',
-          actress: ''
+    export default {
+        name: 'search',
+        components: {
+            preview
+        },
+        data() {
+            return {
+                form: {
+                    jav_code: '',
+                    actress: ''
+                },
+                to_be_previewed: null
+            }
+        },
+        methods: {
+            async search_by_code() {
+                const data = {
+                    'code': this.form.jav_code,
+                    'actress': this.form.actress
+                };
+
+                const rsp = await axios.post("http://localhost:8081/search", data);
+                this.to_be_previewed = rsp.data;
+            },
+
+            clear() {
+                this.form.jav_code = "";
+                this.form.actress = "";
+            }
         }
-      }
-    },
-    methods: {
-      search_by_code() {
-
-        const data = {
-          'code': this.form.jav_code,
-          'actress': this.form.actress
-        };
-
-        axios.post("http://localhost:8081/search", data)
-            .then(function(response){
-                console.log(response);
-            })
-            .catch(function(error){
-                console.log(error);
-            });
-      },
-
-      clear() {
-        this.form.jav_code = "";
-        this.form.actress = "";
-      }
     }
-  }
 
 </script>
 
