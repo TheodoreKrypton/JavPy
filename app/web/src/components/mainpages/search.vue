f42df507ab469972f5310df287d8e7101e3d7761
TheodoreKrypton <624722398@qq.com>
2018-11-27 22:44:57 +0800

    add search by actress on web

--- app/web/src/components/mainpages/search.vue
+++ app/web/src/components/mainpages/search.vue
@@ -1,19 +1,37 @@
 <template>
-    <div>
-        <el-form ref="form" :model="form" :inline="true" @submit="search_by_code">
-            <el-form-item>
-                <el-input v-model="form.jav_code" placeholder="Jav Code"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-input v-model="form.actress" placeholder="Actress"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="search_by_code">Search!</el-button>
-                <el-button @click="clear">Clear</el-button>
-            </el-form-item>
-        </el-form>
-        <preview :av="to_be_previewed"></preview>
-    </div>
+  <div>
+    <el-form ref="form" :model="form" :inline="true" @submit="search_by_code">
+      <el-form-item>
+        <el-input v-model="form.jav_code" placeholder="Jav Code"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="form.actress" placeholder="Actress"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-switch
+          v-model="form.allow_many_actresses"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        ></el-switch>
+      </el-form-item>
+      <el-form-item>
+        <el-input-number
+          v-model="form.up_to"
+          controls-position="right"
+          :min="1"
+          label="Count"
+          size="mini"
+          style="width: 80px"
+        ></el-input-number>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="search_by_code">Search!</el-button>
+        <el-button @click="clear">Clear</el-button>
+      </el-form-item>
+    </el-form>
+    <preview :av="to_be_previewed"></preview>
+  </div>
 </template>
 
 <script>
@@ -30,7 +48,9 @@
             return {
                 form: {
                     jav_code: '',
-                    actress: ''
+                    actress: '',
+                    allow_many_actresses: false,
+                    up_to: 0
                 },
                 to_be_previewed: null
             }
--- app/webserver/app.py
+++ app/webserver/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app, resources=r'/*')
 
 
-@app.route("/search", methods=['POST'])
+@app.route("/search_by_code", methods=['POST'])
 def search():
     params = json.loads(request.data)
 
@@ -16,6 +16,19 @@ def search():
 
     if params["code"]:
         res = [Functions.search_by_code(params["code"]).to_dict()]
+
+    rsp = jsonify(res)
+    rsp.headers["Access-Control-Allow-Origin"] = "*"
+    return rsp
+
+
+@app.route("/search_by_actress", methods=['POST'])
+def search():
+    params = json.loads(request.data)
+    res = []
+
+    if params["actress"]:
+        res = [Functions.search_by_actress(params["actress"], False, 30).to_dict()]
 
     rsp = jsonify(res)
     rsp.headers["Access-Control-Allow-Origin"] = "*"
