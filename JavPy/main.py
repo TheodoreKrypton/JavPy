4b1765ac82876d90c2df47014e467e9c0fbc7c67
TheodoreKrypton <624722398@qq.com>
2019-04-05 07:11:06 +0800

    update setup.py

--- JavPy/main.py
+++ JavPy/main.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 from __future__ import absolute_import, print_function, unicode_literals
-from JavPy.utils.node import Node
 from JavPy.app.webserver import app
 
 
--- setup.py
+++ setup.py
@@ -31,10 +31,16 @@ setup(
         'flask',
         'six',
         'flask-cors',
-        'gevent',
         'lxml',
     ],
     include_package_data=True,
     exclude_package_data={'': ['.gitignore']},
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*"
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
 )
