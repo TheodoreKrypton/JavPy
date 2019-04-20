607f5c9d9459696cf1cb2ba180b4601aa4786113
TheodoreKrypton <wheatcarrier@gmail.com>
2019-04-20 14:15:24 +0800

    make all requests emitted from pookie

--- JavPy/app/web/src/components/utils.js
+++ JavPy/app/web/src/components/utils.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 import config from "../config.js";
 
-let post_with_cookie = function (url, data) {
+let pookie = function (url, data) {
     return axios.post(`http://${config.address}:${config.port}${url}`, data, {
         withCredentials: true
     });
 }
 
-export default post_with_cookie;
+export default pookie;
