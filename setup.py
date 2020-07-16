0016a64dbe545a2c79bcafae1350b437d6ce936f
Wheat Carrier <wheatcarrier@gmail.com>
2020-07-17 04:27:18 +0800

    fix pypi classifier

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     exclude_package_data={"": [".gitignore"]},
     python_requires=">=3.5",
     classifiers=[
-        "License :: OSI Approved :: Apache-2.0 License",
+        "License :: OSI Approved :: Apache Software License v2.0",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
